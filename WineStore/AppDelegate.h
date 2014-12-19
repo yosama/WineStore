d518201996928c7f68d4b43b64e0145d0f9e4cab
Yosamac <yosamac@gmail.com>
2014-12-19 19:02:17 +0100

    Primer commit

---
+++ WineStore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WineStore
+//
+//  Created by Yosnier on 19/12/14.
+//  Copyright (c) 2014 yos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
